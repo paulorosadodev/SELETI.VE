eea53f7cd280ed23487b21d949878238a48897d8
iMiojo <paulohenrique110803@gmail.com>
2022-11-13 19:45:20 -0300

    test

--- seletive/settings.py
+++ seletive/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-deh$jqrhee#zgapq6p+!7mbk5c!k)64ai^^@vk3!r84robz=p@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
