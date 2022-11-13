fdb1bd34a2a3fd988a560aed88971ca25cf9b611
iMiojo <paulohenrique110803@gmail.com>
2022-11-13 20:02:55 -0300

    test

--- seletive/settings.py
+++ seletive/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 from django.contrib.messages import constants
-import django 
+import django_heroku 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +132,5 @@ MESSAGE_TAGS = {
     constants.INFO: 'alert-info',
     constants.WARNING: 'alert-warning'
 }
+
+django_heroku.settings(locals())
